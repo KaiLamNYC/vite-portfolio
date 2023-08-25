1312cd13d8ad63993e5bfdaff66cf1cfc5f622ed
KaiLamNYC <131339372+KaiLamNYC@users.noreply.github.com>
2023-08-25 17:35:02 -0400

    Update portfolio.js

--- minimal-portfolio/src/data/portfolio.js
+++ minimal-portfolio/src/data/portfolio.js
@@ -3,18 +3,18 @@ export default [
 		title: "Dead Man Switch",
 		imgUrl: "/assets/search-rescue.jpg",
 		stack: ["MongoDB", "React", "NodeJS"],
-		link: "https://fkcodes.com",
+		link: "https://google.com",
 	},
 	{
 		title: "Developer Portfolio",
 		imgUrl: "/assets/rodeo.jpg",
 		stack: ["React", "Tailwind", "Vite"],
-		link: "https://fkcodes.com",
+		link: "https://google.com",
 	},
 	{
 		title: "AI GPT Ptoject",
 		imgUrl: "/assets/maxresdefault.jpg",
 		stack: ["Express", "NodeJS", "Tailwind"],
-		link: "https://gptgobrr.com",
+		link: "https://google.com",
 	},
 ];
